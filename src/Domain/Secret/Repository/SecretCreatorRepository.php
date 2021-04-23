1fa5b028ca34e0a50b401b7ee05ddbeb65ff4ac0
= <=>
2021-04-24 00:29:28 +0200

    Setting expiration date depending on secret type.

--- src/Domain/Secret/Repository/SecretCreatorRepository.php
+++ src/Domain/Secret/Repository/SecretCreatorRepository.php
@@ -2,10 +2,9 @@
 
 namespace App\Domain\Secret\Repository;
 
+use ParagonIE\Halite\Primitive\Symmetric;
 use PDO;
 use Ramsey\Uuid\Uuid;
-
-
 
 /**
  * Repository.
@@ -37,12 +36,26 @@ class SecretCreatorRepository
     // public function insertSecret(array $secret): string
     public function insertSecret(array $secret)
     {
+        // $encryption_key = EncryptionKey::generate();
+        // $ciphertext = Symmetric::encrypt($secret['secret'], $secret['key']);
+
+        // $expirationTime = date('Y-m-d H:i:s')
+        //strtotime('+1 hour +20 minutes',strtotime($start))
+
+        $expirationTime = [
+            'once' => null,
+            '1H' => date('Y-m-d H:i:s', strtotime('+1 hour', strtotime(date('Y-m-d H:i:s')))),
+            '24H' => date('Y-m-d H:i:s', strtotime('+1 day', strtotime(date('Y-m-d H:i:s')))),
+            '7D' => date('Y-m-d H:i:s', strtotime('+7 day', strtotime(date('Y-m-d H:i:s')))),
+            '10M' => date('Y-m-d H:i:s', strtotime('+10 minutes', strtotime(date('Y-m-d H:i:s')))),
+        ][$secret['type']];
+
         $row = [
             'uuid' => Uuid::uuid4(),
             'sekret' => $secret['secret'],
             'kod' => $secret['key'],
             'typ' => $secret['type'],
-            'data_wygasniecia' => date("Y/m/d"),
+            'data_wygasniecia' => $expirationTime,
         ];
 
         $sql = 'INSERT INTO secrets SET
@@ -52,7 +65,7 @@ class SecretCreatorRepository
                 typ=:typ,
                 data_wygasniecia=:data_wygasniecia;';
 
-        $cosiek = $this->connection->prepare($sql)->execute($row);
+        $this->connection->prepare($sql)->execute($row);
 
         // return (string) $this->connection->lastInsertId('uuid');
         return $row['uuid'];
